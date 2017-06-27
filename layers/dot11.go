ffb89b7c6e58f67b5bac192424a36d983c5a5a58
Christopher Tran <me@christran.in>
2017-06-26 23:28:33 -0400

    add return for decodeFromBytes

--- layers/dot11.go
+++ layers/dot11.go
@@ -1416,6 +1416,7 @@ func (m *Dot11MgmtAction) DecodeFromBytes(data []byte, df gopacket.DecodeFeedbac
     m.DialogToken = data[2];
     m.TagNumber = data[3];
     m.TagLength = data[4];
+    return nil;
 }
 
 type Dot11MgmtActionNoAck struct {
